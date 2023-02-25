dce28bddac6cb57b1777da01a1422b8d7c57f279
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-02-25 12:15:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+const [inputList, setInputList] = useState("");
+
+const [Items, setItems] = useState([]);
+
+
+const itemEvent=(e)=>{
+   setInputList(e.target.value)
+}
+
+const listOfItems = ()=>{
+   setItems((oldItems)=>{
+    return [...oldItems, inputList]
+   })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>ToDo list</h1>
+        <br />
+        <input type="text" placeholder="Add a item" onChange={itemEvent} />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+         { Items.map((i) => {
+           return <li>{i}</li> 
+          })}
+        </ol>
+      </div>    
     </div>
+    </>
   );
 }
 
